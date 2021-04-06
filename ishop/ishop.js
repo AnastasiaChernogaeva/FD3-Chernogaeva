322adb2a5dd1db8a01bd0449d177c023a10e690b
NastyaCher <sergeychernogaev1979@gmail.com>
2021-04-06 18:59:37 +0300

    commit

---
+++ ishop/ishop.js
@@ -0,0 +1,30 @@
+
+var ISHOP = React.createClass({
+
+    displayName: "ISHOP",
+
+    getDefaultProps: function () {
+        return {
+          shopName: "NAM'S",
+          allItems: itemsArr,
+        };
+      },
+  
+    render: function () {
+
+      var itemsCode=[];
+
+      items.forEach(function(v,i,a){
+          var textAmount='Количество товаров на складе';
+          var textCost='Цена';
+          var item=this.props.allItems[i];
+          var itemCode=
+          React.DOM.tr({key:item.code, className:'tableRow'}, 
+          React.DOM.td({className:'itemPhoto'}, item.itemPhotoURL),
+          React.DOM.td({className:'itemInfo'}, React.DOM.h3(item.itemName), React.DOM.p(textAmount,item.itemAmount),React.DOM.p({className:'cost'},textAmount,item.itemCost) )
+          )
+      })
+      return React.createElement(ISHOP);
+    },
+  
+  })
