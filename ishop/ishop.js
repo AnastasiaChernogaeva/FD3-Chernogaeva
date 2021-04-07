60263170b9727ec0b8d87d19aa39a0f27350c173
NastyaCher <sergeychernogaev1979@gmail.com>
2021-04-07 14:11:50 +0300

    FinalChanges

--- ishop/ishop.js
+++ ishop/ishop.js
@@ -10,19 +10,19 @@ var ISHOP = React.createClass({
       var itemsCode=[];
 
       this.props.allItems.forEach(function(v,i,a){
-          var textAmount='Количество товаров на складе';
-          var textCost='Цена';
+          var textAmount='Количество товаров на складе: ';
+          var textCost='Цена: ';
           var itemCode=
-          React.DOM.tr({key:v.code, className:'tableRow'}, 
+          React.DOM.tr({ className:'tableRow'}, 
           React.DOM.td({className:'itemPhoto'}, React.DOM.img({src:v.itemPhotoURL}) ),
-          React.DOM.td({className:'itemInfo'}, React.DOM.h3(v.itemName), React.DOM.p(textAmount,v.itemAmount),React.DOM.p({className:'cost'},textAmount,v.itemCost) )
+          React.DOM.td({className:'itemInfo'}, React.DOM.h3({className:'nameItem',}, v.itemName), React.DOM.p({className:'amount',}, textAmount,v.itemAmount),React.DOM.p({className:'cost'},textCost,v.itemCost) )
           );
 
           itemsCode.push(itemCode);
       })
       return React.DOM.div( {className:'ISHOP'}, 
-      React.DOM.div( {className:'SHOPNAME'}, this.props.shopName ),
-      React.DOM.table( {className:'ALLITEMS'},React.DOM.tbody(itemsCode) ),
+      React.DOM.h1( {className:'SHOPNAME'}, this.props.shopName ),
+      React.DOM.table( {className:'ALLITEMS'},React.DOM.tbody({className:'tb'}, itemsCode) ),
       );
     },
   
