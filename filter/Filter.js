659b309c935b2098293119bf8d8516f37bc2029d
NastyaCher <sergeychernogaev1979@gmail.com>
2021-04-14 18:32:39 +0300

    hh

--- filter/Filter.js
+++ filter/Filter.js
@@ -1,9 +1,20 @@
 var Filter = React.createClass({
 
     displayName: " Filter ",
+
+
+    propTypes: {
+      workMode: React.PropTypes.number.isRequired,
+      answers:React.PropTypes.arrayOf(string).isRequired
+    },
   
     render: function () {
-      return React.createElement();
+
+      var answersCode=React.createElement(VotesAnswer, {text:this.props.answers.textWords, workMode:this.props.workMode,
+        }
+      );
+
+      return React.createElement(Filter, null, answersCode);
     },
   
   })
