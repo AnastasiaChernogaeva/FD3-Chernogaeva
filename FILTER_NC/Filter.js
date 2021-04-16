f33e5966c5159dcfd93035d01a4941dddee5d146
NastyaCher <sergeychernogaev1979@gmail.com>
2021-04-16 22:15:30 +0300

    Ready1

--- FILTER_NC/Filter.js
+++ FILTER_NC/Filter.js
@@ -18,7 +18,7 @@ var Filter = React.createClass({
     compileList: function(){
         let lines=this.props.arr.slice(); // делаем плоскую копию всех слов, т.к. возможно будем сортировать массив
             if ( this.state.filterString )
-               lines=lines.filter( line => line.indexOf(this.state.filterString)!=-1 );
+               lines=lines.filter( line => line.indexOf(this.state.filterString)!=-1, line+" " );
             if ( this.state.sort )
                lines.sort();
         this.setState( { arr:lines } );
@@ -34,7 +34,7 @@ var Filter = React.createClass({
         return React.DOM.div( {className:'block'}, 
         React.DOM.input({className:'checkbox', type:'checkbox', onClick:this.change,},), 
         React.DOM.input( {className:'text', type:'text', }, ),
-        React.DOM.input( {className:'button', type:'button', placeholder:'Reset', }, ),
+        React.DOM.input( {className:'button', type:'button', defaultValue:'reset', }, ),
         React.DOM.div( {className:'arr'}, this.state.arr  ),
         );
       },
