8b4edff2e5e2cde564d3b6fdd0cced949f8893a6
Nastya <sergeychernogaev1979@gmail.com>
2021-04-23 21:32:48 +0300

    NN

--- ishop-2/ishop2.js
+++ ishop-2/ishop2.js
@@ -8,6 +8,8 @@ var ISHOP = React.createClass({
         cbSelected: React.PropTypes.func.isRequired,
         code:React.PropTypes.number,
         cbDelete:React.PropTypes.func.isRequired,
+        v:React.PropTypes.object,
+        i:React.PropTypes.number,
     },
 
 
@@ -30,11 +32,10 @@ var ISHOP = React.createClass({
       
           var textAmount='Количество товаров на складе: ';
           var textCost='Цена: ';
-          var v=this.state.items[i];
           
-         return  React.DOM.tr({ className:'tableRow', id:v.code, key:v.code, onClick:this.select, }, 
-          React.DOM.td({className:'itemPhoto',  }, React.DOM.img({src:v.itemPhotoURL}) ),
-          React.DOM.td({className:'itemInfo',  }, React.DOM.h3({className:'nameItem',}, v.itemName), React.DOM.p({className:'amount',}, textAmount,v.itemAmount),React.DOM.p({className:'cost'},textCost,v.itemCost) ),
+         return  React.DOM.tr({ className:'tableRow', id:this.props.v.code, key:this.props.v.code, onClick:this.select, }, 
+          React.DOM.td({className:'itemPhoto',  }, React.DOM.img({src:this.props.v.itemPhotoURL}) ),
+          React.DOM.td({className:'itemInfo',  }, React.DOM.h3({className:'nameItem',}, this.props.v.itemName), React.DOM.p({className:'amount',}, textAmount,this.props.v.itemAmount),React.DOM.p({className:'cost'},textCost,this.props.v.itemCost) ),
           React.DOM.td({className:'del', }, React.DOM.input({type:"button", defaultValue:"delete", onClick: this.funDelete,},) )
           );
 
