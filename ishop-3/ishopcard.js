b6b2f15610eb8f1e81b22cdd24ea1844fd365b9e
Nastya <sergeychernogaev1979@gmail.com>
2021-04-30 22:00:55 +0300

    card1

--- ishop-3/ishopcard.js
+++ ishop-3/ishopcard.js
@@ -24,21 +24,19 @@ class IshopCard extends React.Component {
           var textAmount='Количество товаров на складе: ';
           var textCost='Цена: ';
 
-          if (this.props.selectedItem==this.props.v.code){
-            return(
+
+
+           (this.props.selectedItem==this.props.v.code)?return(
               <div className='card'  id={this.props.v.code} key={this.props.v.code}> 
                 <div className='itemPhoto'><img  src= {this.props.v.itemPhotoURL}/></div> 
                 <div className='itemInfo'> 
                     <h3 className='nameItem'>{this.props.v.itemName}</h3>
-                    <p className='amount'>{textAmount,this.props.v.itemAmount}</p>
-                    <p className='cost'>{textCost,this.props.v.itemCost}</p>
+                    <p className='amount'>textAmount{this.props.v.itemAmount}</p>
+                    <p className='cost'>textCost{this.props.v.itemCost}</p>
                 </div>
             </div>
-            );
-        }
-         else{
-         return ();
-        }
+            ):null;
+        
 
     };
   
