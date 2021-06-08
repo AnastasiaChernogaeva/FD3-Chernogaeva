c5b323f2b1caf925c088949ee28bf383e6cf04b9
Nastya <sergeychernogae1979@gmail.com>
2021-06-08 10:58:00 +0300

    New_One

--- Scales/app.ts
+++ Scales/app.ts
@@ -2,21 +2,21 @@
 class Scales {
 
     static productsCount:number=0;
-    products:Array<Product>;
-    scale:number;
+    products:Array<Product>=[];
+   /* scale:number;
     name:string;
 
     constructor() {
         Scales.productsCount++;
      
-    }
+    }*/
 
     add(_newProduct:Product):void {
         this.products.push(_newProduct);
     };
 
     getSumScale():number {
-        let sumScale=0;
+        let sumScale:number=0;
         this.products.forEach(elem=>{sumScale+=elem.scale});
         return sumScale;
     }
@@ -35,19 +35,21 @@ class Product {
     scale:number;
     name:string;
 
-    constructor(_name:string, _scale:number) {
+    constructor( _scale:number,_name:string,) {
         this.name=_name;
         this.scale=_scale;
 
 
     }
 
-    getScale(_scale:number):void {
+    getScale(_scale:number):number {
         console.log(` ${this.name}: вес-${_scale} грамм `);
+    return  this.scale;
     }
 
-     getName(_name:string):void {
+     getName(_name:string):string {
         console.log(` Название продукта ${_name} `);
+        return  this.name;
     }
     
 }
@@ -56,10 +58,10 @@ class Apple extends Product {
 
     species:string;
 
-    constructor(_species:string) {
+    constructor( scale:number,_species:string) {
         // конструктор класса-потомка должен вызвать 
         // конструктор класса-предка
-        super(); 
+        super(scale, "apple"); 
         this.species=_species;
     }
 
@@ -73,10 +75,10 @@ class Tomato extends Product {
  
     species:string;
 
-    constructor(_species:string) {
+    constructor(scale:number,_species:string) {
         // конструктор класса-потомка должен вызвать 
         // конструктор класса-предка
-        super(); 
+        super(scale, "tomato"); 
         this.species=_species;
     }
 
@@ -85,32 +87,32 @@ class Tomato extends Product {
     }
 }
 
-let Apple1:Apple=new Apple("фрукт");
+let Apple1:Apple=new Apple(60,"фрукт");
 Apple1.show();
 Apple1.getName("Яблоко");
 Apple1.getScale(60);
 
-let Apple2:Apple=new Apple("фрукт");
+let Apple2:Apple=new Apple(140,"фрукт");
 Apple2.show();
 Apple2.getName("Яблоко");
 Apple2.getScale(140);
 
-let Apple3:Apple=new Apple("фрукт");
+let Apple3:Apple=new Apple(140,"фрукт");
 Apple3.show();
 Apple3.getName("Яблоко");
 Apple3.getScale(140);
 
-let Tomato1:Tomato=new Tomato("овощь");
+let Tomato1:Tomato=new Tomato(60,"овощь");
 Tomato1.show();
 Tomato1.getName("Помидор");
 Tomato1.getScale(60);
 
-let Tomato2:Tomato=new Tomato("овощь");
+let Tomato2:Tomato=new Tomato(120,"овощь");
 Tomato2.show();
 Tomato2.getName("Помидор");
 Tomato2.getScale(120);
 
-let Tomato3:Tomato=new Tomato("овощь");
+let Tomato3:Tomato=new Tomato(180,"овощь");
 Tomato3.show();
 Tomato3.getName("Помидор");
 Tomato3.getScale(180);
