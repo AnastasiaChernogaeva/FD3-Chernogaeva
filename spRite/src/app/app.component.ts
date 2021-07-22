c6879669c905a6687f3b5059e022e2878fc27072
Nastya <sergeychernogaev1979@gmail.com>
2021-07-22 23:38:42 +0300

    L_Y

--- spRite/src/app/app.component.ts
+++ spRite/src/app/app.component.ts
@@ -9,10 +9,55 @@ export class AppComponent {
   title = 'app';
 
 
-@Output("clicked")
-public clicked:EventEmitter<string>=new EventEmitter<string>();
+  
+  private photoSprite:string="  http://fe.it-academy.by/Examples/cards2.png  ";
 
-getClicked(){
-  this.clicked.emit('clicked');
-}
+  private widthWhole:number=574;
+
+  private heightWhole:number=2712;
+ 
+  private width:number=138;
+
+  private height:number=186;
+
+  private offsetX:number=2;
+
+  private offsetY:number=2;
+
+  getPhoto():string {
+    return this.photoSprite;
+  };
+
+
+  getWidth():string {
+    let meanWidth=`${this.width}+px`;
+    return meanWidth;
+  };
+
+  getHeight():string{
+    let meanHeight=`${this.height}+px`;
+    return meanHeight;
+  }
+
+  getOffset():string{
+    if(this.width+this.offsetX<this.widthWhole){
+      this.offsetX+=this.width;
+    }
+    let width=`${this.offsetX}+px`;
+    if(this.height+this.offsetY<this.heightWhole){
+      this.offsetY+=this.height;
+    }
+    let height=`${this.offsetY}+px`;
+
+    let row=width+height;
+    return row;
+
+  };
+
+// @Output("clicked")
+// public clicked:EventEmitter<string>=new EventEmitter<string>();
+
+// getClicked(){
+//   this.clicked.emit('clicked');
+// }
 }
--- spRite/src/app/sprite.component.ts
+++ spRite/src/app/sprite.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter,Output } from '@angular/core';
 
 @Component({
   selector: 'sprite',
@@ -11,49 +11,56 @@ export class SpriteComponent {
 
   title = 'sprite';
 
-  private photoSprite:string="  http://fe.it-academy.by/Examples/cards2.png  ";
+  // private photoSprite:string="  http://fe.it-academy.by/Examples/cards2.png  ";
 
-  private widthWhole:number=574;
+  // private widthWhole:number=574;
 
-  private heightWhole:number=2712;
+  // private heightWhole:number=2712;
  
-  private width:number=138;
+  // private width:number=138;
 
-  private height:number=186;
+  // private height:number=186;
 
-  private offsetX:number=2;
+  // private offsetX:number=2;
 
-  private offsetY:number=2;
+  // private offsetY:number=2;
 
-  getPhoto():string {
-    return this.photoSprite;
-  };
+  // getPhoto():string {
+  //   return this.photoSprite;
+  // };
 
 
-  getWidth():string {
-    let meanWidth=`${this.width}+px`;
-    return meanWidth;
-  };
+  // getWidth():string {
+  //   let meanWidth=`${this.width}+px`;
+  //   return meanWidth;
+  // };
 
-  getHeight():string{
-    let meanHeight=`${this.height}+px`;
-    return meanHeight;
-  }
+  // getHeight():string{
+  //   let meanHeight=`${this.height}+px`;
+  //   return meanHeight;
+  // }
 
-  getOffset():string{
-    if(this.width+this.offsetX<this.widthWhole){
-      this.offsetX+=this.width;
-    }
-    let width=`${this.offsetX}+px`;
-    if(this.height+this.offsetY<this.heightWhole){
-      this.offsetY+=this.height;
-    }
-    let height=`${this.offsetY}+px`;
+  // getOffset():string{
+  //   if(this.width+this.offsetX<this.widthWhole){
+  //     this.offsetX+=this.width;
+  //   }
+  //   let width=`${this.offsetX}+px`;
+  //   if(this.height+this.offsetY<this.heightWhole){
+  //     this.offsetY+=this.height;
+  //   }
+  //   let height=`${this.offsetY}+px`;
 
-    let row=width+height;
-    return row;
+  //   let row=width+height;
+  //   return row;
 
-  };
+  // };
+
+  @Output("clicked")
+public clicked:EventEmitter<string>=new EventEmitter<string>();
+
+getClicked(){
+  this.clicked.emit('clicked');
+}
 
  
 }
