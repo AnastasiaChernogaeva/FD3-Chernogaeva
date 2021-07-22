04a6d2eb73a880f6e4777703f77b6a1e51010355
Nastya <sergeychernogaev1979@gmail.com>
2021-07-22 22:09:19 +0300

    SS

--- attempt/src/app/app.component.ts
+++ attempt/src/app/app.component.ts
@@ -9,11 +9,26 @@ import { Component, Output, EventEmitter } from '@angular/core';
 export class AppComponent {
   title = 'app';
 
+  num:number=0;
 
-@Output("clicked")
-public clicked:EventEmitter<string>=new EventEmitter<string>();
+  getNum():number{
+    return this.num;
+  }
 
-getClicked(){
-  this.clicked.emit('clicked');
-}
+  getClicked(mean:string):void{
+    if(mean==='-'){
+      let newNum=this.num-1;
+      this.num=newNum;
+    }
+    else if(mean==='+'){
+      let newNum=this.num+1;
+      this.num=newNum;
+    }
+  }
+// @Output("clicked")
+// public clicked:EventEmitter<string>=new EventEmitter<string>();
+
+// getClicked(){
+//   this.clicked.emit('clicked');
+// }
 }
--- attempt/src/app/app.module.ts
+++ attempt/src/app/app.module.ts
@@ -2,11 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { SpriteComponent } from './sprite.component';
+// import { SpriteComponent } from './sprite.component';
 
 @NgModule({
   declarations: [
-    AppComponent, SpriteComponent,
+    AppComponent
   ],
   imports: [
     BrowserModule
